1f5bfd457cb7da54843f5899277139c74efd808e
Yosiya Hinosawa <stibium121@gmail.com>
2013-09-16 00:38:22 +0900

    Fix test

--- src/test/java/org/kt3k/straw/plugin/BasicPluginsTest.java
+++ src/test/java/org/kt3k/straw/plugin/BasicPluginsTest.java
@@ -16,7 +16,7 @@ public class BasicPluginsTest {
 			"org.kt3k.straw.plugin.HttpPlugin",
 			"org.kt3k.straw.plugin.LogPlugin",
 			"org.kt3k.straw.plugin.OptionsMenuPlugin",
-			"org.kt3k.straw.plugin.SharedPreferencePlugin",
+			"org.kt3k.straw.plugin.SharedPreferencesPlugin",
 			"org.kt3k.straw.plugin.UIPlugin",
 		}, BasicPlugins.names);
 	}
