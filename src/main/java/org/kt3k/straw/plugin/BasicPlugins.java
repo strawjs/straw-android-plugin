ef9d0e4ea92bacda871e74ffdccbbe0000021bd9
Yosiya Hinosawa <stibium121@gmail.com>
2013-09-16 00:36:52 +0900

    Fix plugin name

--- src/main/java/org/kt3k/straw/plugin/BasicPlugins.java
+++ src/main/java/org/kt3k/straw/plugin/BasicPlugins.java
@@ -7,8 +7,17 @@ public class BasicPlugins {
 		"org.kt3k.straw.plugin.HttpPlugin",
 		"org.kt3k.straw.plugin.LogPlugin",
 		"org.kt3k.straw.plugin.OptionsMenuPlugin",
-		"org.kt3k.straw.plugin.SharedPreferencePlugin",
+		"org.kt3k.straw.plugin.SharedPreferencesPlugin",
 		"org.kt3k.straw.plugin.UIPlugin",
+	};
+
+	public static Class<?>[] classes = new Class<?>[]{
+		ActivityPlugin.class,
+		HttpPlugin.class,
+		LogPlugin.class,
+		OptionsMenuPlugin.class,
+		SharedPreferencesPlugin.class,
+		UIPlugin.class,
 	};
 
 }
