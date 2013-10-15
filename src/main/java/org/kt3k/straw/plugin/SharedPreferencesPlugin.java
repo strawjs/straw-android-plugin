513fccc70c3fee05da4ce5b5fee34679acf137cb
Yosiya Hinosawa <stibium121@gmail.com>
2013-10-15 13:28:00 +0900

    Modify spacings

--- src/main/java/org/kt3k/straw/plugin/SharedPreferencesPlugin.java
+++ src/main/java/org/kt3k/straw/plugin/SharedPreferencesPlugin.java
@@ -14,28 +14,41 @@ public class SharedPreferencesPlugin extends StrawPlugin {
 		return "sharedPreferences";
 	}
 
+
 	private SharedPreferences getDefaultPrefs() {
 		return PreferenceManager.getDefaultSharedPreferences(this.context);
 	}
 
+
 	public static class KeyParam {
 		public String key;
 	}
+
 
 	public static class KeyValueParam {
 		public String key;
 		public String value;
 	}
 
-	public static class GetParam extends KeyValueParam {}
 
-	public static class SetParam extends KeyValueParam {}
+	public static class GetParam extends KeyValueParam {
+	}
 
-	public static class RemoveParam extends KeyParam {}
 
-	public static class HasParam extends KeyParam {}
+	public static class SetParam extends KeyValueParam {
+	}
+
+
+	public static class RemoveParam extends KeyParam {
+	}
+
+
+	public static class HasParam extends KeyParam {
+	}
+
 
 	public static class SharedPreferencesResult {
+
 		public String value;
 
 		public SharedPreferencesResult() {
@@ -44,45 +57,52 @@ public class SharedPreferencesPlugin extends StrawPlugin {
 		public SharedPreferencesResult(String value) {
 			this.value = value;
 		}
+
 	}
+
 
 	@PluginAction
 	public void dump(Object _, StrawDrink drink) {
 		drink.success(new SharedPreferencesResult(this.getDefaultPrefs().getAll().toString()));
 	}
 
+
 	@PluginAction
 	public void set(SetParam param, StrawDrink drink) {
 		Boolean result = this.getDefaultPrefs().edit().putString(param.key, param.value).commit();
-		
+
 		drink.success(new SharedPreferencesResult(result.toString()));
 	}
+
 
 	@PluginAction
 	public void get(GetParam param, StrawDrink drink) {
 		String result = this.getDefaultPrefs().getString(param.key, param.value);
-		
+
 		drink.success(new SharedPreferencesResult(result));
 	}
+
 
 	@PluginAction
 	public void remove(RemoveParam param, StrawDrink drink) {
 		Boolean result = this.getDefaultPrefs().edit().remove(param.key).commit();
-		
+
 		drink.success(new SharedPreferencesResult(result.toString()));
 	}
+
 
 	@PluginAction
 	public void has(HasParam param, StrawDrink drink) {
 		Boolean result = this.getDefaultPrefs().contains(param.key);
-		
+
 		drink.success(new SharedPreferencesResult(result.toString()));
 	}
+
 
 	@PluginAction
 	public void clear(Object _, StrawDrink drink) {
 		Boolean result = this.getDefaultPrefs().edit().clear().commit();
-		
+
 		drink.success(new SharedPreferencesResult(result.toString()));
 	}
 }
