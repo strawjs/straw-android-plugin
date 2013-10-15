c09faddaee4772815357fa2aa1912a014306969d
Yosiya Hinosawa <stibium121@gmail.com>
2013-10-15 22:49:35 +0900

    Add tests of UIPlugin

--- src/test/java/org/kt3k/straw/plugin/UIPluginTest.java
+++ src/test/java/org/kt3k/straw/plugin/UIPluginTest.java
@@ -2,6 +2,13 @@ package org.kt3k.straw.plugin;
 
 import org.junit.runner.RunWith;
 import org.junit.Test;
+import org.junit.Before;
+
+import static org.mockito.Mockito.*;
+
+import org.kt3k.straw.StrawDrink;
+import static org.kt3k.straw.plugin.UIPlugin.*;
+
 import static org.junit.Assert.*;
 
 import org.robolectric.RobolectricTestRunner;
@@ -9,9 +16,32 @@ import org.robolectric.RobolectricTestRunner;
 @RunWith(RobolectricTestRunner.class)
 public class UIPluginTest {
 
+	UIPlugin plugin;
+	ToastParam param;
+	StrawDrink drink;
+
+	@Before
+	public void setUp() {
+		plugin = new UIPlugin();
+		param = new ToastParam();
+		drink = mock(StrawDrink.class);
+	}
+
+
 	@Test
 	public void testGetName() {
 		assertEquals("ui", new UIPlugin().getName());
+	}
+
+
+	@Test
+	public void testToast() {
+
+		param.text = "abc";
+
+		plugin.toast(param, drink);
+		plugin.toastLong(param, drink);
+
 	}
 
 }
